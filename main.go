476f6668c4f85c8eb3835fa614f72a0338f26638
Ben Lu <hilltop16@gmail.com>
2018-04-11 18:36:07 +0800

    accept arguments for host and guest

--- main.go
+++ main.go
@@ -1,10 +1,35 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
 	var isHost bool
 
 	flag.BoolVar(&isHost, "listen", false, "listen on the specified ip")
 	flag.Parse()
+
+	if isHost {
+		fmt.Println("is host")
+		//the third argument, after main, and the flag
+		connIP := os.Args[2]
+		runHost(connIP)
+	} else {
+		fmt.Println("is guest")
+		//the second argument because there is no flag in the guest side
+		connIP := os.Args[1]
+		runGuest(connIP)
+	}
+}
+
+
+func runHost(ip string) {
+
+}
+
+func runGuest(ip string) {
+
 }
