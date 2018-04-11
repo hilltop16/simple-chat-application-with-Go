b5c18f66c0c4c6751cb43de692dc6568ec71434f
Ben Lu <hilltop16@gmail.com>
2018-04-11 18:58:07 +0800

    implemented host listener function

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"net"
+	"bufio"
 )
 
 func main() {
@@ -15,18 +17,38 @@ func main() {
 	if isHost {
 		fmt.Println("is host")
 		//the third argument, after main, and the flag
-		connIP := os.Args[2]
+		connIP := os.Args[2] + ":8080"
 		runHost(connIP)
 	} else {
 		fmt.Println("is guest")
 		//the second argument because there is no flag in the guest side
-		connIP := os.Args[1]
+		connIP := os.Args[1] + ":8080"
 		runGuest(connIP)
 	}
 }
 
 
 func runHost(ip string) {
+	listen, listenErr :=net.Listen("tcp", ip)
+	if listenErr != nil {
+		fmt.Println("Error:",listenErr)
+		os.Exit(1)
+	}
+	//Accept method returns a conn object
+	conn, acceptErr:=listen.Accept()
+	if acceptErr != nil {
+		fmt.Println("Error:",acceptErr)
+		os.Exit(1)
+	}
+	//create a reader interface
+	reader:=bufio.NewReader(conn)
+	//convert to string
+	message, readErr:=reader.ReadString('\n') // \n = enter key
+	if readErr != nil {
+		fmt.Println("Error:",readErr)
+		os.Exit(1)
+	}
+	fmt.Println(message)
 
 }
 
