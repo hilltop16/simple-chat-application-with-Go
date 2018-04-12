dc164444c70f1920dd48aefde1efcc9b2f93539a
Ben Lu <hilltop16@gmail.com>
2018-04-12 23:32:46 +0800

    refactor for loop, added handle message function for host and guest

--- lib/main.go
+++ lib/main.go
@@ -18,25 +18,30 @@ func RunHost(ip string) {
 	if acceptErr != nil {
 		log.Fatal("Error:", acceptErr)
 	}
+
+	for {
+		handleHost(conn)
+	}
+}
+
+func handleHost(conn net.Conn) {
 	//create a reader interface
 	reader := bufio.NewReader(conn)
-	for {
-		//convert to string
-		message, readErr := reader.ReadString('\n') // \n = enter key
-		if readErr != nil {
-			log.Fatal("Error", readErr)
-		}
-		fmt.Println("message received:", message)
-
-
-		fmt.Printf("Send message:")
-		replyReader :=bufio.NewReader(os.Stdin)
-		replyMessage, replyReadErr:=replyReader.ReadString('\n')
-		if replyReadErr != nil {
-			log.Fatal("error:", readErr)
-		}
-		fmt.Fprint(conn, replyMessage)
+	//convert to string
+	message, readErr := reader.ReadString('\n') // \n = enter key
+	if readErr != nil {
+		log.Fatal("Error", readErr)
 	}
+	fmt.Println("message received:", message)
+
+
+	fmt.Printf("Send message:")
+	replyReader :=bufio.NewReader(os.Stdin)
+	replyMessage, replyReadErr:=replyReader.ReadString('\n')
+	if replyReadErr != nil {
+		log.Fatal("error:", readErr)
+	}
+	fmt.Fprint(conn, replyMessage)
 }
 
 func RunGuest(ip string) {
@@ -46,14 +51,18 @@ func RunGuest(ip string) {
 		log.Fatal("Error:",dialErr)
 	}
 	for {
-		fmt.Print("Send message:")
-		reader:=bufio.NewReader(os.Stdin)
-		message, readErr:=reader.ReadString('\n')
-		if readErr != nil {
-			log.Fatal("Error", readErr)
-		}
-		fmt.Fprint(conn, message)
+		handleGuest(conn)
 	}
 
 
+}
+
+func handleGuest(conn net.Conn) {
+	fmt.Print("Send message:")
+	reader:=bufio.NewReader(os.Stdin)
+	message, readErr:=reader.ReadString('\n')
+	if readErr != nil {
+		log.Fatal("Error", readErr)
+	}
+	fmt.Fprint(conn, message)
 }
